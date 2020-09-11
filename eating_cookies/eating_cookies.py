84af057eba6475d243a848a3d7b123c93b65ff5b
robertjallen <bobbidigi34@gmail.com>
2020-09-10 22:36:25 -0400

    add eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,24 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
+def eating_cookies(n, cache=None):
     # Your code here
+    if cache is None:
+        cache = {}
+    if n == 0 or n == 1:
+        return 1
+    if n == 2:
+        return 2
+    if n == 3:
+        return 4
+    
+    if cache and cache[n]:
+        return cache[n]
+    
+    cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    
+    return cache[n]
 
-    pass
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
